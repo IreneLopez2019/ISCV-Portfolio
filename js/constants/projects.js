03392e9a99ad3b17eaaf22f383e8fc6fd47bee83
Irene <irene@miracledevs.com>
2020-03-06 08:14:30 -0300

    rendir

--- js/constants/projects.js
+++ js/constants/projects.js
@@ -1,68 +1,71 @@
 export const ilustracionProjects = [
-    {
-        id: "mi-tia-chus-project",
-        name: "Mi tía Chus",
-        mainImage: "assets/img/imagenes/mi_tia_chus.png",
-        otherImages: [
-            "assets/img/imagenes/cuervos-07.png",
-            "assets/img/imagenes/aspronaga-07.png",
-        ],
-        description:
-            "Mi tía Chus fue un proyecto Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, harum doloremque. Iusto quis obcaecati vitae nobis facilis esse soluta reprehenderit minima in, corrupti nam accusamus praesentium, eum repellendus magnam ducimus!"
-    },
-    {
-        id: "pictionary-project",
-        name: "Pictonary",
-        mainImage: "assets/img/imagenes/pictionary.png",
-        otherImages: [
-            "assets/img/imagenes/cartas-07.png",
-            "assets/img/imagenes/cartas-08.png",
-            "assets/img/imagenes/cartas-09.png",
-        ],
+	{
+		id: "mi-tia-chus-project",
+		name: "Mi tía Chus",
+		mainImage: "assets/img/imagenes/mi_tia_chus.png",
+		otherImages: [
+			"assets/img/imagenes/cuervos-07.png",
+			"assets/img/imagenes/aspronaga-07.png"
+		],
+		description:
+			"Mi tía Chus fue un proyecto realizado en cuarto año en el cual rediseñé las ilustraciones de una nota de la revista Orsai. La técnica elegida fue collage digital."
+	},
+	{
+		id: "pictionary-project",
+		name: "Pictonary",
+		mainImage: "assets/img/imagenes/pictionary.png",
+		otherImages: [
+			"assets/img/imagenes/cartas-07.png",
+			"assets/img/imagenes/cartas-08.png",
+			"assets/img/imagenes/cartas-09.png"
+		],
 
-        otherOtherImages: [
-            "assets/img/imagenes/tapa-pictionary_1.png",
-            "assets/img/imagenes/caja-juego_1.png",
-        ],
+		otherOtherImages: [
+			"assets/img/imagenes/tapa-pictionary_1.png",
+			"assets/img/imagenes/caja-juego_1.png"
+		],
 
-        description:
-            "Pictionary fue un proyecto Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, harum doloremque. Iusto quis obcaecati vitae nobis facilis esse soluta reprehenderit minima in, corrupti nam accusamus praesentium, eum repellendus magnam ducimus!"
-    },
-    {
-        id: "ilustración-tridimensional",
-        name: "Ilustración tridimensional",
-        mainImage: "assets/img/imagenes/meduza-grilla.png",
-        description:
-            "Mi tía Chus fue un proyecto Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, harum doloremque. Iusto quis obcaecati vitae nobis facilis esse soluta reprehenderit minima in, corrupti nam accusamus praesentium, eum repellendus magnam ducimus!"
-    },
+		description:
+			"Proyecto realizado en la materia Ilustración 3, en el cual debíamos rediseñar un juego de mesa a elección. Las ilustraciones fueron realizadas en Adobe Illustrator y Photoshop."
+	},
+	{
+		id: "ilustración-tridimensional",
+		name: "Ilustración tridimensional",
+		mainImage: "assets/img/imagenes/meduza-grilla.png",
+		description:
+			"Ilustración tridimensional. Esta ilustración fue realizada utilizando basura y otros recursos de luces para retratar en una foto mi preocupación sobre la contaminación actual."
+	}
 ];
 
 export const brandingProjects = [
-    {
-        name: "Rediseño Trumpeter",
-        mainImage: "assets/img/imagenes/grilla-vinos.png",
-     
-        description:
-        "Mi tía Chus fue un proyecto Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, harum doloremque. Iusto quis obcaecati vitae nobis facilis esse soluta reprehenderit minima in, corrupti nam accusamus praesentium, eum repellendus magnam ducimus!"
-    },
-    {
-        name: "Project 2 - Branding",
-        image:
-            "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4f18b992206863.5e4560580cd12.jpg",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis illo quidem quod vitae voluptates ratione sapiente temporibus adipisci, nemo cumque dicta ad et fugit maiores ullam! Necessitatibus accusantium deserunt dolore quas laborum voluptatibus accusamus fuga atque assumenda, perferendis sed expedita possimus aliquam cupiditate doloremque asperiores provident qui veritatis, dicta cum consequatur animi quia. Velit repellat modi perferendis autem. Quia illo corrupti voluptates, non ipsa laboriosam voluptatum sunt, quos eaque dolorum repellat neque itaque quibusdam amet veritatis praesentium ex. Voluptate, nisi reprehenderit facere consectetur esse similique expedita a nemo voluptatum at quod mollitia. Possimus, consequuntur nesciunt labore esse provident at?"
-    }
+	{
+		name: "Rediseño Trumpeter",
+		mainImage: "assets/img/imagenes/grilla-vinos.png",
+
+		description:
+			"Rediseño Trumpeter fue realizado en la materia Diseño 4, bajo la premisa de convertir una marca de vinos que ,en la realidad, apunta a un público de mediana edad a un público atractivo para los jóvenes. Las ilustraciones geométricas que predominan en el diseño fueron realizadas en Adobe Illustrator."
+	},
+	{
+		name: "Project 2 - Branding",
+		image:
+			"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/4f18b992206863.5e4560580cd12.jpg",
+		description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis illo quidem quod vitae voluptates ratione sapiente temporibus adipisci, nemo cumque dicta ad et fugit maiores ullam! Necessitatibus accusantium deserunt dolore quas laborum voluptatibus accusamus fuga atque assumenda, perferendis sed expedita possimus aliquam cupiditate doloremque asperiores provident qui veritatis, dicta cum consequatur animi quia. Velit repellat modi perferendis autem. Quia illo corrupti voluptates, non ipsa laboriosam voluptatum sunt, quos eaque dolorum repellat neque itaque quibusdam amet veritatis praesentium ex. Voluptate, nisi reprehenderit facere consectetur esse similique expedita a nemo voluptatum at quod mollitia. Possimus, consequuntur nesciunt labore esse provident at?"
+	}
 ];
 
 export const editorialProjects = [
-    {
-        name: "Project 1 - Editorial",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis illo quidem quod vitae voluptates ratione sapiente temporibus adipisci, nemo cumque dicta ad et fugit maiores ullam! Necessitatibus accusantium deserunt dolore quas laborum voluptatibus accusamus fuga atque assumenda, perferendis sed expedita possimus aliquam cupiditate doloremque asperiores provident qui veritatis, dicta cum consequatur animi quia. Velit repellat modi perferendis autem. Quia illo corrupti voluptates, non ipsa laboriosam voluptatum sunt, quos eaque dolorum repellat neque itaque quibusdam amet veritatis praesentium ex. Voluptate, nisi reprehenderit facere consectetur esse similique expedita a nemo voluptatum at quod mollitia. Possimus, consequuntur nesciunt labore esse provident at?"
-    },
-    {
-        name: "Project 2 - Editorial",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia veritatis illo quidem quod vitae voluptates ratione sapiente temporibus adipisci, nemo cumque dicta ad et fugit maiores ullam! Necessitatibus accusantium deserunt dolore quas laborum voluptatibus accusamus fuga atque assumenda, perferendis sed expedita possimus aliquam cupiditate doloremque asperiores provident qui veritatis, dicta cum consequatur animi quia. Velit repellat modi perferendis autem. Quia illo corrupti voluptates, non ipsa laboriosam voluptatum sunt, quos eaque dolorum repellat neque itaque quibusdam amet veritatis praesentium ex. Voluptate, nisi reprehenderit facere consectetur esse similique expedita a nemo voluptatum at quod mollitia. Possimus, consequuntur nesciunt labore esse provident at?"
-    }
+	{
+		name: "RawPower",
+		mainImage: "assets/img/imagenes/editorial3.png",
+		otherImages: ["assets/img/imagenes/editorial2.png"],
+		description:
+			"RawPower fue un rediseño de la revista Rolling Stones llevada a cabo en segundo año de la facultad, en la materia Editorial. Se diseño desde su marca hasta su puesta en página. "
+	},
+	{
+		name: "Aquabar",
+		mainImage: "assets/img/imagenes/Aquabar.png",
+		description:
+			"Carta Aquabar fue un proyecto que realice en el año 2018 trabajando en una agencia de Rosario. Aquabar es un bar situado en el acuario de la ciudad. Respete la identidad de su marca para diseñar esta carta edición primavera verano."
+	}
 ];
